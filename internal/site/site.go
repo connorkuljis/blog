e1b9b654f344acd31c945f338748736355a31c1e
connorkuljis <conkuljis@gmail.com>
2024-12-24 23:01:54 +0800

    update site package, use page interface

--- internal/site/site.go
+++ internal/site/site.go
@@ -1,11 +1,13 @@
 package site
 
 import (
-	"bytes"
 	"html/template"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 
+	"github.com/connorkuljis/content/internal/model"
 	"github.com/connorkuljis/content/internal/store"
 	"github.com/jmoiron/sqlx"
 	"github.com/yuin/goldmark"
@@ -13,6 +15,15 @@ import (
 
 type Site struct {
 	Title string
+
+	DB             *sqlx.DB
+	MarkdownParser goldmark.Markdown
+}
+
+type Page interface {
+	Filepath() string
+	Data() map[string]any
+	TemplateName() string
 }
 
 func (s *Site) Init() error {
@@ -23,6 +34,7 @@ func (s *Site) Init() error {
 	}
 	os.MkdirAll("public", os.ModePerm)
 
+	// copy all static directory contents into public
 	err = os.CopyFS("public", os.DirFS("static"))
 	if err != nil {
 		return err
@@ -31,126 +43,30 @@ func (s *Site) Init() error {
 	return nil
 }
 
-func (s *Site) Render(db *sqlx.DB, lib *HTMLTemplateLibrary, md goldmark.Markdown) error {
-	err := s.Init()
+func (s *Site) Render() error {
+	tpls, err := template.ParseGlob("templates/*.html")
 	if err != nil {
 		return err
 	}
 
-	entryPageView := PageView{
-		Name:               "entry.html",
-		BaseTemplate:       lib.Base["base.html"],
-		LayoutTemplate:     lib.Base["layout.html"],
-		HeadTemplate:       lib.Base["head.html"],
-		ComponentTemplates: lib.AllComponents(),
-		ViewTemplate:       lib.Views["entry.html"],
-	}
-	entryTemplate, err := entryPageView.Template()
+	categories, err := store.NewCategoryRepository(s.DB).ReadAllCategoriesWithEntries()
 	if err != nil {
 		return err
 	}
 
-	err = s.RenderEntry("public", db, md, entryTemplate)
-	if err != nil {
-		return err
-	}
-
-	categoryPageView := PageView{
-		Name:               "category.html",
-		BaseTemplate:       lib.Base["base.html"],
-		LayoutTemplate:     lib.Base["layout.html"],
-		HeadTemplate:       lib.Base["head.html"],
-		ComponentTemplates: lib.AllComponents(),
-		ViewTemplate:       lib.Views["category.html"],
-	}
-	categoryTemplate, err := categoryPageView.Template()
-	if err != nil {
-		return err
-	}
-
-	err = s.RenderCategory("public", db, categoryTemplate)
-	if err != nil {
-		return err
-	}
-
-	indexPageView := PageView{
-		Name:               "index.html",
-		BaseTemplate:       lib.Base["base.html"],
-		LayoutTemplate:     lib.Base["layout.html"],
-		HeadTemplate:       lib.Base["head.html"],
-		ComponentTemplates: lib.AllComponents(),
-		ViewTemplate:       lib.Views["index.html"],
-	}
-	indexTemplate, err := indexPageView.Template()
-	if err != nil {
-		return err
-	}
-
-	err = s.RenderIndex("public", db, indexTemplate)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (s *Site) RenderEntry(dir string, db *sqlx.DB, md goldmark.Markdown, t *template.Template) error {
-	entries, err := store.NewEntryRepository(db).ReadAllJoinCategories()
-	if err != nil {
-		return err
-	}
-
-	for _, entry := range entries {
-		filepath := filepath.Join(dir, slugify(entry.CategoryTitle), slugify(entry.Title), "index.html")
-
-		page := Page{
-			Title:    entry.Title,
-			Filepath: filepath,
-			T:        t,
-		}
-
-		// convert markdown to html
-		var buf bytes.Buffer
-		if err := md.Convert([]byte(entry.Content), &buf); err != nil {
-			panic(err)
-		}
-
-		data := map[string]any{
-			"Site":    s,
-			"Entry":   entry,
-			"Content": template.HTML(buf.String()),
-		}
-
-		err := page.Render(data)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (s *Site) RenderCategory(dir string, db *sqlx.DB, t *template.Template) error {
-	categories, err := store.NewCategoryRepository(db).ReadAllCategoriesWithEntries()
-	if err != nil {
-		return err
+	pages := []Page{
+		HomePage{Categories: categories},
 	}
 
 	for _, category := range categories {
-		filepath := filepath.Join(dir, slugify(category.Title), "index.html")
-
-		page := Page{
-			Title:    category.Title,
-			Filepath: filepath,
-			T:        t,
+		pages = append(pages, CategoryPage{Category: category})
+		for _, entry := range category.Entries {
+			pages = append(pages, EntryPage{Category: category, Entry: entry})
 		}
+	}
 
-		data := map[string]any{
-			"Site":     s,
-			"Category": category,
-		}
-
-		err := page.Render(data)
+	for _, page := range pages {
+		err := Render(tpls, page)
 		if err != nil {
 			return err
 		}
@@ -159,27 +75,91 @@ func (s *Site) RenderCategory(dir string, db *sqlx.DB, t *template.Template) err
 	return nil
 }
 
-func (s *Site) RenderIndex(dir string, db *sqlx.DB, t *template.Template) error {
-	categories, err := store.NewCategoryRepository(db).ReadAllCategoriesWithEntries()
+func Render(tpls *template.Template, page Page) error {
+	dir, _ := filepath.Split(page.Filepath())
+	os.MkdirAll(dir, os.ModePerm)
+
+	f, err := os.Create(page.Filepath())
 	if err != nil {
 		return err
 	}
 
-	page := Page{
-		Title:    "index.html",
-		Filepath: filepath.Join(dir, "index.html"),
-		T:        t,
-	}
-
-	data := map[string]any{
-		"Site":       s,
-		"Categories": categories,
-	}
-
-	err = page.Render(data)
+	err = tpls.ExecuteTemplate(f, page.TemplateName(), page.Data())
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+type HomePage struct {
+	Categories []model.Category
+}
+
+func (p HomePage) Filepath() string {
+	return "public/index.html"
+}
+
+func (p HomePage) TemplateName() string {
+	return "view-index.html"
+}
+
+func (p HomePage) Data() map[string]any {
+	return map[string]any{
+		"Categories": p.Categories,
+	}
+}
+
+type CategoryPage struct {
+	Category model.Category
+}
+
+func (p CategoryPage) Filepath() string {
+	return filepath.Join("public", p.Category.Title, "index.html")
+}
+
+func (p CategoryPage) TemplateName() string {
+	return "view-category.html"
+}
+
+func (p CategoryPage) Data() map[string]any {
+	return map[string]any{
+		"Category": p.Category,
+	}
+}
+
+type EntryPage struct {
+	Category model.Category
+	Entry    model.Entry
+}
+
+func (p EntryPage) Filepath() string {
+	return filepath.Join("public", p.Category.Title, p.Entry.Title, "index.html")
+}
+
+func (p EntryPage) TemplateName() string {
+	return "view-entry.html"
+}
+
+func (p EntryPage) Data() map[string]any {
+	return map[string]any{
+		"Entry": p.Entry,
+	}
+}
+
+var funcMap = template.FuncMap{
+	"slugify": slugify,
+}
+
+func slugify(s string) string {
+	// Convert to lowercase
+	s = strings.ToLower(s)
+
+	// Replace non-alphanumeric characters with a hyphen
+	s = regexp.MustCompile(`[^a-z0-9]+`).ReplaceAllString(s, "-")
+
+	// Remove leading and trailing hyphens
+	s = strings.Trim(s, "-")
+
+	return s
 }
