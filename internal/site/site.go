b194d1b159a57e1e72b2120c69c1ecf97f91c366
connorkuljis <conkuljis@gmail.com>
2025-01-05 15:45:52 +0800

    checkpoint

--- internal/site/site.go
+++ internal/site/site.go
@@ -13,17 +13,20 @@ import (
 	"github.com/yuin/goldmark"
 )
 
+var funcMap = template.FuncMap{
+	"slugify":  util.Slugify,
+	"truncate": util.Truncate,
+}
+
 type Site struct {
 	Title      string
 	Categories []model.Category
+	Entries    []model.Entry
 
 	DB             *sqlx.DB
 	MarkdownParser goldmark.Markdown
-}
 
-type NavItem struct {
-	Name     string
-	Resource string
+	Publish bool
 }
 
 func (s *Site) Init() error {
@@ -43,50 +46,50 @@ func (s *Site) Init() error {
 	return nil
 }
 
+func filterPublishedEntries(entries []model.Entry) []model.Entry {
+	var publishedEntries []model.Entry
+	for _, entry := range entries {
+		if entry.Publish == 1 {
+			publishedEntries = append(publishedEntries, entry)
+		}
+
+	}
+	return publishedEntries
+}
+
 func (s *Site) Render() error {
-	categories, err := store.NewCategoryRepository(s.DB).ReadAllCategories()
+	categoryRepo := store.NewCategoryRepository(s.DB)
+	categories, err := categoryRepo.ReadAllCategories()
 	if err != nil {
 		return err
 	}
-
 	s.Categories = categories
 
-	publishedEntries, err := store.NewEntryRepository(s.DB).ReadPublishedEntries()
+	entryRepo := store.NewEntryRepository(s.DB)
+	siteEntries, err := entryRepo.ReadAllEntries()
 	if err != nil {
 		return err
 	}
+	s.Entries = siteEntries
 
-	recentEntries, err := store.NewEntryRepository(s.DB).ReadRecentlyPublishedEntries(10)
-	if err != nil {
-		return err
+	if s.Publish {
+		s.Entries = filterPublishedEntries(siteEntries)
 	}
 
 	pages := []Page{
 		HomePage{
-			Site:          s,
-			RecentEntries: recentEntries,
+			Site: s,
 		},
 	}
 
-	for _, entry := range publishedEntries {
-		// parse markdown to html
-		var buf bytes.Buffer
-		err := s.MarkdownParser.Convert([]byte(entry.Content), &buf)
+	for _, category := range categories {
+		entries, err := entryRepo.ReadAllByCategoryID(category.ID)
 		if err != nil {
 			return err
 		}
 
-		pages = append(pages, EntryPage{
-			Site:    s,
-			Entry:   entry,
-			Content: template.HTML(buf.String()),
-		})
-	}
-
-	for _, category := range categories {
-		entries, err := store.NewEntryRepository(s.DB).ReadPublishedEntriesByCategoryID(category.ID)
-		if err != nil {
-			return err
+		if s.Publish {
+			entries = filterPublishedEntries(siteEntries)
 		}
 
 		pages = append(pages, CategoryPage{
@@ -94,11 +97,24 @@ func (s *Site) Render() error {
 			Category: category,
 			Entries:  entries,
 		})
-	}
 
-	funcMap := template.FuncMap{
-		"slugify":  util.Slugify,
-		"truncate": util.Truncate,
+		// build the entry pages and parse md content to templatable safe HTML
+		for _, entry := range entries {
+			// parse markdown to html
+			var buf bytes.Buffer
+			err := s.MarkdownParser.Convert([]byte(entry.Content), &buf)
+			if err != nil {
+				return err
+			}
+			content := template.HTML(buf.String())
+
+			pages = append(pages, EntryPage{
+				Site:         s,
+				Category:     category,
+				CurrentEntry: entry,
+				Content:      content,
+			})
+		}
 	}
 
 	tpls, err := template.New("").Funcs(funcMap).Option("missingkey=error").ParseGlob("templates/*.html")
@@ -125,7 +141,7 @@ func renderPage(tpls *template.Template, page Page) error {
 		return err
 	}
 
-	err = tpls.ExecuteTemplate(f, page.TemplateName(), page.Data())
+	err = tpls.ExecuteTemplate(f, page.TemplateName(), page) // note: render page struct directly into the template data.
 	if err != nil {
 		return err
 	}
