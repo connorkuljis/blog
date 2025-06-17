3ccafaf968bcc08eaaab74fdb318134bd485e98a
connorkuljis <conkuljis@gmail.com>
2025-06-17 20:49:05 +0800

    scripts: simplify deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with non-zero status
 
-# Variables
-REMOTE_HOST="${SSH_USER}@${SERVER_IP}"
-STAGING_DIR="~/public/"
-DOCUMENT_ROOT="/var/www/html/kuljis.xyz"
-LOCAL_DIR="www_root/"
-
-# Copy local assets to remote staging directory
 echo "Syncing local assets to staging directory..."
-rsync --delete -avz ${LOCAL_DIR} ${REMOTE_HOST}:${STAGING_DIR}
 
-# Use a single SSH connection with sudo privileges
-echo "Deploying from staging to document root..."
+rsync --delete -avz \
+	public/ \
+	prod@kuljis.xyz:/home/prod/www/blog/public/
 
-ssh -t ${REMOTE_HOST} \
-	"sudo bash -c 'rm -rf ${DOCUMENT_ROOT}/* && cp -r ${STAGING_DIR}* ${DOCUMENT_ROOT}/'"
-
-echo "Deployment completed successfully!"
