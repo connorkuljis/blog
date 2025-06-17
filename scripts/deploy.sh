0c69bb7b7dbd1619de26d655cd105bb2fc33425f
connorkuljis <conkuljis@gmail.com>
2025-06-17 21:09:46 +0800

    scripts: update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ set -e  # Exit immediately if a command exits with non-zero status
 echo "Syncing local assets to staging directory..."
 
 rsync --delete -avz \
-	public/ \
-	prod@kuljis.xyz:/home/prod/www/blog/public/
+	dist/ \
+	prod@kuljis.xyz:/home/prod/www/kuljis.xyz/dist/
 
