afe538bee29782dec191723e18f16c5c7242d105
connorkuljis <conkuljis@gmail.com>
2025-06-18 15:41:14 +0800

    makefile: move shell scrips to makefile

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e  # Exit immediately if a command exits with non-zero status
-
-echo "Syncing local assets to staging directory..."
-
-rsync --delete -avz \
-	dist/ \
-	prod@kuljis.xyz:/home/prod/www/kuljis.xyz/dist/
-
